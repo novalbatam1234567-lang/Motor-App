ce76b867cc1de14201c3b1bebdc58918b1e45fd4
Novriyandri <novalbatam1234567@gmail.com>
2026-07-17 14:51:02 +0700

    fix: remove xampp config to fix deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Enable static export untuk XAMPP Apache
-  basePath: "/Motor", // Set basePath untuk folder Motor di XAMPP
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  }
+  // Konfigurasi standar untuk Vercel
+  reactStrictMode: true,
 };
 
 export default nextConfig;
