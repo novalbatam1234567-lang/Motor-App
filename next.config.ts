29c4cf20b1f926253e52b5399347b6fc0d84c86b
Novriyandri <novalbatam1234567@gmail.com>
2026-07-17 13:16:07 +0700

    fix: update next.config.ts for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/Motor",
+  // output: "export", // Hapus atau comment baris ini agar aplikasi berjalan di mode serverless
+  // basePath: "/Motor", // Hapus atau comment agar aplikasi terbuka di halaman utama (root)
   images: {
     unoptimized: true,
   }
